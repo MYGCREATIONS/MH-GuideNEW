df013298e5cbbcbd421629b9674bb7c1e2d931e5
ygohar <yousuf.gohar@molecularhealth.com>
2018-11-12 18:45:45 +0100

    remove comments

--- features/stepDefinitions/phenotypeVariantsStepDefinitions.py
+++
@@ -1,8 +0,0 @@
-from lettuce import step
-from Framework.pages.cviPage import CreateCvi
-from Framework.pages.dashboardPage import Dashboard
-
-
-dashboard = Dashboard()
-
-@step('I click on \[(.*)\]')
